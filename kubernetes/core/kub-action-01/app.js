9cc2baf170986ad04e1b1dd9431b86f5f86490ee
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2023-02-12 18:53:16 +0800

    Kubernetes01-update02

--- kubernetes/core/kub-action-01/app.js
+++ kubernetes/core/kub-action-01/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const  os = reqire('os');
+const hostname = os.hostname();
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <p>my name is: ${hostname}</p>
   `);
 });
 
