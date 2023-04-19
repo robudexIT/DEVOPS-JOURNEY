b425b66ac848af468ddfa1f69c0a6eb1e9819e77
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2023-04-19 22:25:21 +0800

    add jenkins project

---
+++ jenkins/Jenkins_Upgradev3/maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+
+  
+  public static void main(String args[]){
+    
+	  System.out.println("Hello from Main Method!!!");
+	  System.out.println("Edit by Rogmer Bulaclac for devops study purpose");
+  }
+}
